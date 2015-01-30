d557b27f68d22da09704ff5a453be1bbc29be4ec
kenpeter <figo2476@hotmail.com>
2015-01-30 17:07:53 +1100

    Hide user account url

--- modules/custom/intranet/js/intranet.js
+++ modules/custom/intranet/js/intranet.js
@@ -2,12 +2,11 @@
 
 	Drupal.behaviors.zintranet = {
   	attach: function (context, settings) {
-			//console.log(context);		
-			//console.log(settings);
-			$(document).ready(function () {
-				var element_id = 'li.admin-menu-action.admin-menu-account > a';
-				$(element_id).attr('href', 'javascript:void(0);');
-			});
+			// NOTE: it is not working reliably.
+			//$(document).ready(function () {
+			//	var element_id = 'li.admin-menu-action.admin-menu-account > a';
+			//	$(element_id).attr('href', 'javascript:void(0);');
+			//});
   	}
 	};
 
