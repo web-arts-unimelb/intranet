6bac866f651fbce83bbd80f72724fab0fcea3fe1
kenpeter <figo2476@hotmail.com>
2015-08-26 10:55:07 +1000

    ssl causes search bar issue

--- modules/custom/intranet/js/intranet.js
+++ modules/custom/intranet/js/intranet.js
@@ -1,12 +1,23 @@
 (function ($) {
 
-	Drupal.behaviors.zintranet = {
+	Drupal.behaviors.intranet = {
   	attach: function (context, settings) {
 			// NOTE: it is not working reliably.
 			//$(document).ready(function () {
 			//	var element_id = 'li.admin-menu-action.admin-menu-account > a';
 			//	$(element_id).attr('href', 'javascript:void(0);');
 			//});
+
+			// NOTE: after siwtching to ssl, the <div id=""> of search bar, it should be <div id="arts-staff-dropdown-site-search">
+			// Buy why?
+			$(document).ready(function(){
+				var class_id = ".col-2.rightside.float_right";
+				var needed_id = "arts-staff-dropdown-site-search";
+				$(class_id).attr("id", needed_id);
+
+				console.log('test');	
+			});
+
   	}
 	};
 
